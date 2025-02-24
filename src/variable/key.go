feb7db6f0cc6a76bcc85218e86dec58ef08b4acd
PARKNAMSU <skatn7979@gmail.com>
2025-02-24 06:53:42 +0000

    key setting

--- src/variable/key.go
+++ src/variable/key.go
@@ -3,5 +3,7 @@ package variable
 import "os"
 
 var (
-	USER_TOKEN_KEY = os.Getenv("JWT_USER_TOKEN_KEY")
+	USER_TOKEN_KEY            = os.Getenv("JWT_USER_TOKEN_KEY")
+	REFRESH_TOKEN_ENCRYPT_KEY = os.Getenv("REFRESH_TOKEN_ENCRYPT_KEY")
+	USER_PASSWORD_ENCRYPT_KEY = os.Getenv("USER_PASSWORD_ENCRYPT_KEY")
 )
