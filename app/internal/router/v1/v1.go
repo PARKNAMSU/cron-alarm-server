18c7f4dfefd6f5521ce8e2478363bee411c3885d
PARKNAMSU <skatn7979@gmail.com>
2025-02-25 04:06:46 +0000

    user router

--- app/internal/router/v1/v1.go
+++ app/internal/router/v1/v1.go
@@ -7,5 +7,6 @@ import (
 func APIV1Router() func(router fiber.Router) {
 	return func(router fiber.Router) {
 		router.Route("/sample", SampleV1Router())
+		router.Route("/user", UserRouter())
 	}
 }
